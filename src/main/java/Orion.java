df90c0699c487e4ac49ae319df65f70e0bf13f43
Vansh Puri <purivansh@gmail.com>
2026-01-30 03:34:15 +0800

    Level-1: echo

--- src/main/java/Orion.java
+++ src/main/java/Orion.java
@@ -1,12 +1,35 @@
-public class Orion {
-    public static void main(String[] args) {
-        String line = "____________________________________________________________";
+import java.util.Scanner;
 
-        System.out.println(line);
+public class Orion {
+    private static final String LINE = "____________________________________________________________";
+
+    public static void main(String[] args) {
+        greet();
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                printBoxed("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            printBoxed(input);
+        }
+    }
+
+    private static void greet() {
+        System.out.println(LINE);
         System.out.println(" Hello! I'm Orion");
         System.out.println(" What can I do for you?");
-        System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(LINE);
+    }
+
+    private static void printBoxed(String message) {
+        System.out.println(LINE);
+        System.out.println(" " + message);
+        System.out.println(LINE);
+        System.out.println();
     }
 }
